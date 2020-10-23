83f34b67d958e4434519b787d3a354c470444caa
ichhoa129 <ichhoa129@gmail.com>
2020-10-24 02:40:51 +0700

    add some bugs

--- b7.js
+++ b7.js
@@ -19,6 +19,7 @@ function quickTest(){
         assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
         assert.strictEqual(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
         assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+        assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456 7890"); // wrong syntax at this line 
         console.log('ok');
     } catch (error) {
         console.error(error);
